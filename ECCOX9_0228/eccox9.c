4da15fa5e0f0e0df2a58eec68db3644c11499d0d
HZsolt2024 <zsolthercegkuti@gmail.com>
2025-03-03 18:43:00 +0100

    .

--- ECCOX9_0228/eccox9.c
+++ ECCOX9_0228/eccox9.c
@@ -4,40 +4,53 @@
 
 #define MAX_LINE 50
 
-int main() {
+void nagybeture(const char *line)
+{
+    while (*line)
+    {
+        putchar(toupper(*line));
+        line++;
+    }
+}
+
+int main()
+{
     char filename[] = "eccox9.txt";
     char line[MAX_LINE];
     FILE *file;
-    
 
     file = fopen(filename, "w");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Hiba a fájl megnyitásakor");
         return 1;
     }
-    
+
     printf("Add meg a neved és a neptunkódodat. A befejezéshez írjd be: #\n");
-    
-    while (1) {
+
+    while (1)
+    {
         fgets(line, MAX_LINE, stdin);
         if (line[0] == '#' && line[1] == '\n')
             break;
         fputs(line, file);
     }
-    
+
     fclose(file);
-    
+
     file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Hiba a fájl megnyitásakor");
         return 1;
     }
-    
+
     printf("\nA fájl tartalma:\n");
-    while (fgets(line, MAX_LINE, file) != NULL) {
-        printf("%s", line);
+    while (fgets(line, MAX_LINE, file) != NULL)
+    {
+        nagybeture(line);
     }
-    
+
     fclose(file);
     return 0;
 }
